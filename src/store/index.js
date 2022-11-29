f11086b27c67f8feaaa4b33ad906a8d85159910f
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2022-11-29 09:22:35 +0300

    добавлен id в список

--- src/store/index.js
+++ src/store/index.js
@@ -26,16 +26,20 @@ export default new Vuex.Store({
                             date: '28.03.2020',
                             category: 'Food',
                             value: 169,
+                            id: 1
                         },
                         {
                             date: '24.03.2020',
                             category: 'Transport',
                             value: 360,
+                            id: 2
+
                         },
                         {
                             date: '24.03.2020',
                             category: 'Food',
                             value: 532,
+                            id: 3
                         },
                     ])
                 }, 1000)
